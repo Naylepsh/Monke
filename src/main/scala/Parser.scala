4f4825742d9943cff5f9fd21c1046a8863459074
Naylepsh <naylepsh@gmail.com>
2023-06-25 18:47:17 +0200

    add identifier, int and prefix operator expressions

--- src/main/scala/Parser.scala
+++ src/main/scala/Parser.scala
@@ -2,15 +2,20 @@ object Parser:
   enum ParsingError:
     case InvalidInteger(value: String)             extends ParsingError
     case InvalidLetExpression(tokens: List[Token]) extends ParsingError
+    case InvalidExpression(tokens: List[Token])    extends ParsingError
+    case NoPrefixExpression(token: Token)          extends ParsingError
     case UnmatchedCase(tokens: List[Token])        extends ParsingError
 
   enum Expression:
-    case Identifier          extends Expression
-    case Integer(value: Int) extends Expression
+    case Identifier(value: String) extends Expression
+    case Integer(value: Int)       extends Expression
+    case PrefixOperator(token: Token, expression: Expression)
+        extends Expression
 
   enum Statement:
     case Let(identifier: String, expression: Expression) extends Statement
     case Return(expression: Expression)                  extends Statement
+    case Expr(expression: Expression)                    extends Statement
 
   type Node = Expression | Statement
 
@@ -26,6 +31,8 @@ object Parser:
       tokens match
         case Nil | (Token.EOF :: Nil) =>
           if errors.isEmpty then Right(ast.reverse) else Left(errors.reverse)
+        // Keep it for now, until the rest of the parsing is not fully fleshed out
+        case Token.Semicolon :: rest => doParse(rest, ast, errors)
         case all @ Token.Let
             :: Token.Identifier(identifier)
             :: Token.Assign
@@ -48,14 +55,35 @@ object Parser:
             case Right((expr, leftoverTokens)) =>
               doParse(leftoverTokens, Statement.Return(expr) :: ast, errors)
         case other =>
-          val (failedTokens, leftoverTokens) = eatUntilExprEnd(other)
-          doParse(
-            leftoverTokens,
-            ast,
-            ParsingError.UnmatchedCase(failedTokens) :: errors
-          )
+          parseExpressionV2(other) match
+            case Left(reason) =>
+              val (failedTokens, leftoverTokens) = eatUntilExprEnd(other)
+              doParse(leftoverTokens, ast, reason :: errors)
+            case Right((expression, leftoverTokens)) =>
+              doParse(leftoverTokens, Statement.Expr(expression) :: ast, errors)
 
     doParse(tokens, List.empty, List.empty)
+
+  private def parseExpressionV2(tokens: List[Token])
+      : Either[ParsingError, (Expression, List[Token])] =
+    parsePrefixExpression(tokens)
+
+  private def parsePrefixExpression(tokens: List[Token])
+      : Either[ParsingError, (Expression, List[Token])] =
+    tokens match
+      case Nil => Left(ParsingError.InvalidExpression(Nil))
+      case Token.Identifier(value) :: rest =>
+        Right(Expression.Identifier(value) -> rest)
+      case Token.Integer(value) :: rest =>
+        value.toIntOption match
+          case None      => Left(ParsingError.InvalidInteger(value))
+          case Some(int) => Right(Expression.Integer(int) -> rest)
+      case (token @ (Token.Bang | Token.Minus)) :: rest =>
+        parseExpressionV2(rest) match
+          case reason @ Left(_) => reason
+          case Right((expression, leftoverTokens)) =>
+            Right(Expression.PrefixOperator(token, expression), leftoverTokens)
+      case token :: rest => Left(ParsingError.NoPrefixExpression(token))
 
   private def parseExpression(tokens: List[Token])
       : Either[ParsingError, (Expression, List[Token])] =
--- src/test/scala/ParserSuite.scala
+++ src/test/scala/ParserSuite.scala
@@ -28,7 +28,7 @@ class ParserSuite extends munit.FunSuite:
 
     assertEquals(ast.left.map(_.length), Left(3))
 
-  test("Can parse return statement"):
+  test("Parse return statement"):
     val input = """return 5;
     |return 10;
     |return 993322;""".stripMargin
@@ -41,4 +41,46 @@ class ParserSuite extends munit.FunSuite:
     val tokens = Lexer.tokenize(input)
     val ast    = parse(tokens)
 
-    assertEquals(ast.left.map(_.length), expected)
+    assertEquals(ast, expected)
+
+  test("Parse identifier expression statement"):
+    val input    = "foobar;"
+    val expected = Right(List(Statement.Expr(Expression.Identifier("foobar"))))
+
+    val tokens = Lexer.tokenize(input)
+    val ast    = parse(tokens)
+
+    assertEquals(ast, expected)
+
+  test("Parse integer expression statement"):
+    val input    = "42;"
+    val expected = Right(List(Statement.Expr(Expression.Integer(42))))
+
+    val tokens = Lexer.tokenize(input)
+    val ast    = parse(tokens)
+
+    assertEquals(ast, expected)
+
+  test("Parse prefix expression"):
+    val input = "!42;"
+    val expected = Right(List(Statement.Expr(Expression.PrefixOperator(
+      Token.Bang,
+      Expression.Integer(42)
+    ))))
+
+    val tokens = Lexer.tokenize(input)
+    val ast    = parse(tokens)
+
+    assertEquals(ast, expected)
+
+  test("Parse prefix expression"):
+    val input = "-42;"
+    val expected = Right(List(Statement.Expr(Expression.PrefixOperator(
+      Token.Minus,
+      Expression.Integer(42)
+    ))))
+
+    val tokens = Lexer.tokenize(input)
+    val ast    = parse(tokens)
+
+    assertEquals(ast, expected)
