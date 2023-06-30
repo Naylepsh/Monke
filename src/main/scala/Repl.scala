16ad1caced7ec04ba593ddce42a083fb3d5c9e6b
Naylepsh <naylepsh@gmail.com>
2023-06-30 20:47:02 +0200

    turn rlpl to rppl

--- src/main/scala/Repl.scala
+++ src/main/scala/Repl.scala
@@ -1,9 +1,16 @@
 import scala.io.StdIn.readLine
+import Show.*
+import AST.Node.given
 
 object Repl:
   @annotation.tailrec
   def run: Unit =
     val line   = readLine(">>")
     val tokens = Lexer.tokenize(line)
-    tokens.foreach(println)
+    val ast = Parser.parse(tokens)
+
+    ast match
+      case Left(errors) => errors.foreach(println)
+      case Right(program) => println(program.show)
+
     run
