ee24d2968d729e36150989aefffad03ab7b14d39
Naylepsh <naylepsh@gmail.com>
2023-07-02 16:58:56 +0200

    turn rppl into repl

--- src/main/scala/MonkeyObject.scala
+++ src/main/scala/MonkeyObject.scala
@@ -2,3 +2,11 @@ enum MonkeyObject:
   case IntegerLiteral(value: Int)     extends MonkeyObject
   case BooleanLiteral(value: Boolean) extends MonkeyObject
   case Null                           extends MonkeyObject
+
+object MonkeyObject:
+  given Show[MonkeyObject] with
+    def show(o: MonkeyObject): String =
+      o match
+        case IntegerLiteral(value) => value.toString
+        case BooleanLiteral(value) => value.toString
+        case Null                  => "null"
--- src/main/scala/Repl.scala
+++ src/main/scala/Repl.scala
@@ -1,16 +1,17 @@
 import scala.io.StdIn.readLine
 import Show.*
 import AST.Node.given
+import MonkeyObject.given
 
 object Repl:
   @annotation.tailrec
   def run: Unit =
     val line   = readLine(">>")
     val tokens = Lexer.tokenize(line)
-    val ast = Parser.parse(tokens)
+    val ast    = Parser.parse(tokens)
 
     ast match
-      case Left(errors) => errors.foreach(println)
-      case Right(program) => println(program.show)
+      case Left(errors)   => errors.foreach(println)
+      case Right(program) => println(Eval.eval(program).show)
 
     run
